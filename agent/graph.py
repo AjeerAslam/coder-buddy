f08f47749d7c5a9650887a61e03cf863cb8dd276
ajeer <ajeeraslamk@gmail.com>
2026-02-20 10:57:53 +0530

    gdfgd

--- agent/graph.py
+++ agent/graph.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from langchain.globals import set_verbose, set_debug
+# from langchain.globals import set_verbose, set_debug
 from langchain_groq.chat_models import ChatGroq
 from langgraph.constants import END
 from langgraph.graph import StateGraph
@@ -11,8 +11,8 @@ from agent.tools import write_file, read_file, get_current_directory, list_files
 
 _ = load_dotenv()
 
-set_debug(True)
-set_verbose(True)
+# set_debug(True)
+# set_verbose(True)
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
